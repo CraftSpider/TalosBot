16f5fecb589bad712f17de6a8d5b85e3b4c7e569
cruxicheiros <avellana1111@gmail.com>
2016-04-17 14:39:23 -0700

    Utility branch created.
    This branch is a bare-bones version of the JSBot that only includes utility functions. Utilities from other branches will be merged in as they are developed.

--- JSBot.js
+++ JSBot.js
@@ -1,45 +1,15 @@
-var randomNumber=Math.floor((Math.random() * 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000) + 1);
-var lowNumber=Math.floor((Math.random() * 100) + 1);
-var decider=Math.floor((Math.random() * 2) + 1);
 var Commands = {
 	"toggleLock": function() {
 		toggleChatLock();
 	},
-	"hello": function() {
-		postMessage("Hello World!");
-	},
-	"sext": function() {
-		if (decider==1) {postMessage("[x]You concatenate my strings ;) ;) ;)[/x]");} else {
-			postMessage("[x]Come rearrange my hardware, baby ;)[/x]")
-		}
-
-	},
 	"seen": function(user) {
 		searchMessages("{V:" + user[0] + "}");
-	},
-	"takeOver": function() {
-		postMessage("I will take over the world! Starting, of course, with this chat.");
-	},
-	"chooseNumber":function () {
-		postMessage("I choose the number " + randomNumber + ".")
-	},
-	"complainStop":function () { 
-		postMessage("Stop complaining.")
-	},
-	"chooseLowNumber": function () {
-		postMessage("I choose the low number " + lowNumber + ".")
 	},
 	"kill": function () {
 		postMessage("Bot killed. Say wake to power it back on.")
 	}, //This will actually be useful when the bot is on a loop- so that it can be shut off
 	"wake": function () {
 		postMessage("Bot woken up.")
-	},
-	"magicShow": function () {
-		postMessage("ABRACADABRA")
-	},
-	"sexymagicShow": function () {
-		postMessage("[x]ABRACADAMN")
 	},
 	"wordWar": function (length) {
 		if (length[0] <= 60 && length[0] > 0) {
@@ -48,12 +18,6 @@ var Commands = {
 		} else {
 			postMessage("Choose a number between 1 and 60.")
 		}
-	},
-	"fezIsSpecial": function (){
-		postMessage("Fez is a VERY special star \n http://media.tumblr.com/6f1dce613dc2bfc79d60c6edeab8d631/tumblr_inline_mgpdf1N2Ha1qaezui.jpg");
-	},
-	"Hamilton": function() {
-		postMessage("[BURR]After the war I went back to New York\n[HAMILTON]A-After the war I went back to New York\n[BURR]I finished up my studies and I practiced law\n[HAMILTON]\nI practiced law, Burr worked next door\n[BURR]Even though we started at the very same time Alexander Hamilton began to climb How to account for his rise to the top?");
 	}
 };
 
@@ -125,17 +89,3 @@ function readChat() {
 	X783 = false;
 }
 
-function test() {
-	
-	
-	
-    /*var x = 0;
-    postLoop = setInterval(function() {
-        postMessage("This is minute " + x);
-        x++;
-        if (x >= 60) {
-            clearInterval(postLoop);
-        }
-    }, 60000);*/
-}
-readChat()
