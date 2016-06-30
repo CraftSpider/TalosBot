96294327b05613113f3c01f1cc0045442b9a2cde
wundrweapon <wundrweapon@users.noreply.github.com>
2016-06-30 15:05:09 -0400

    Looks like I used Java syntax. Woops!

--- Talos.js
+++ Talos.js
@@ -131,10 +131,10 @@ var Commands = {
 				postMessage("wundr's not good at making up dumb things for Talos to say, despite the fact that he himself speaks exclusively in stupid");
 				break;
 			case 6:
-				var alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+				var alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
 				var arbitraryLetters = "";
 				
-				for(int i = 0; i < 10; i++) {
+				for(var i = 0; i < 10; i++) {
 					var index = parseInt(Math.floor(Math.random() * 26));
 					arbitraryLetters += alphabet[index];
 				}
