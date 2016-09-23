4b9f530b1a51202d6cb90bae7ffcae81b3185e79
Rune Tynan <runetynan@gmail.com>
2016-09-22 20:10:18 -0400

    Changed to use the ChatzyWrappers.js file.

--- Talos.js
+++ Talos.js
@@ -11,12 +11,6 @@ const WH_TIME = 0;
 const ADMINS = ["Dino", "α|CraftSpider|Ω", "HiddenStorys"];
 const EGG_DEV = "wundrweapon" //this is just here so wundrweapon feels good about himself
 
-//Chatzy Variables
-var messageTable = "X8414";
-var popup = "X5721";
-var messageTime = "X2087";
-var messageButton = "X5794"
-
 //Command variables
 var NumWWs = 0;
 var MaxWWs = 10;
@@ -271,101 +265,6 @@ var ADMIN_COMMANDS = {
 };
 
 /*
-    -----------------
-    Wrapper Functions
-    -----------------
-*/
-function elementByID(elementID) {
-    return document.getElementById(elementID);
-}
-
-function elementsByClass(elementClass) {
-	return document.getElementsByClassName(elementClass);
-}
-
-function leaveChat() {
-    X2661('X3836');
-}
-
-function closePopup() {
-	X8071();
-}
-
-function postMessage(message) {
-    var HTMLTags = ["<b>", "</b>", "<i>", "</i>", "<s>", "</s>", "<u>", "</u>"];
-    var ChatzyTags = ["[b]", "[/b]", "[i]", "[/i]", "[s]", "[/s]", "[u]", "[/u]"];
-    for (var tag in HTMLTags) {
-        message = message.replace(HTMLTags[tag],ChatzyTags[tag]);
-    }
-    X4727(message);
-}
-
-function closeChat() {
-	postMessage("/close");
-}
-
-function openChat() {
-	postMessage("/open");
-}
-
-function toggleChatLock() {
-	if(X5170.X5289) { //Variable for whether the chat is locked, of course.
-		postMessage("/open");
-	} else {
-		postMessage("/close");
-	}
-}
-
-function searchMessages(term, poster) {
-	postMessage("/find " + (poster?"{V:" + poster + "} ":"") + (term?term:""));
-}
-
-function privateMessage(name, message) {
-	postMessage("/pm \"" + name + "\" " + message);
-}
-
-function globalMessage(message) { //Note, only sends the message to online users.
-	var users = X3884.split("\n");
-	for (var i = 1; i <= users[0]; i++) {
-		user = users[i].split("	");
-		privateMessage(user[0], message);	//Replace the 0 with other numbers to grab different values. 2 is last leave/exit, 4 is status, 5 is location.
-	}
-}
-
-function editRoomBoard(message, method, key) {  //Method is the style of editing to use. Options are: 0/default, overwrite. 1, append. 2, prepend. 3, replace.
-    postMessage("/rb");
-    setTimeout(function() {
-        var BoardMessage = elementByID("X6196");
-        switch (method) {
-            case 1:
-                BoardMessage.value = BoardMessage.value + "\n" + message;
-                break;
-            case 2:
-                BoardMessage.value = message + "\n" + BoardMessage.value;
-                break;
-            case 3:
-                if (BoardMessage.value.match(new RegExp(key, "g")).length > 1) {
-                    BoardMessage.value = BoardMessage.value.replace(new RegExp(key + ".+?" + key, "g"), key + message + key);
-                }
-                break;
-            default:
-                BoardMessage.value = message;
-        }
-        X565.onclick();
-    }, 150);
-}
-
-//requires re-init of JSBot. Automate that?
-function changeName(name) {
-	X8752('X6610');
-
-	setTimeout(function() {
-		X4964.value = name;
-		X2575.onsubmit();
-	}, 1000);
-}
-
-/*
 	-------------------
 	Arbitrary functions
 	-------------------
@@ -485,7 +384,19 @@ function mainLoop() {
     Initialization Code
     -------------------
 */
-elementByID(messageTable).innerHTML = '<P class="b">Previous messages parsed (press ESC to re-parse page)</P>\n';
-X8614 = false;
-setInterval(function() {mainLoop();}, 1000);
-setInterval(function() {postMessage("");}, 60000*10);
+function talosInit() {
+	ChatzyAPI = document.createElement('script')
+	ChatzyAPI.setAttribute('type', 'text/javascript')
+	ChatzyAPI.setAttribute('src', 'https://rawgit.com/CraftSpider/TalosBot/update/ChatzyWrappers.js')
+	ChatzyAPI.setAttribute('onload', 'talosStart()')
+	document.head.appendChild(ChatzyAPI)
+}
+
+function talosStart() {
+	elementByID(messageTable).innerHTML = '<P class="b">Previous messages parsed (press ESC to re-parse page)</P>\n';
+	X8614 = false;
+	setInterval(function() {mainLoop();}, 1000);
+	setInterval(function() {postMessage("");}, 60000*10);
+}
+
+talosInit()
