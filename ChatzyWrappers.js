bc006f091787edd299a2ab158d2785be2dbfdbe0
Rune Tynan <runetynan@gmail.com>
2016-11-15 15:29:29 -0500

    Update to new Chatzy version.
    Internal new version name: Chatzy-18

--- ChatzyWrappers.js
+++ ChatzyWrappers.js
@@ -4,13 +4,13 @@
     ---------------------
 */
 
-var messageTable = "X8414";
-var messageContainer = "X4233"
-var popup = "X5721";
-var messageTime = "X2087";
-var messageButton = "X5794";
-var PMTag = 'X1046';
-var textBox = 'X5999';
+var messageTable = "X2325";
+var messageContainer = "X1506"
+var popup = "X7376";
+var messageTime = "X4177";
+var messageButton = "X1602";
+var PMTag = "X6200";
+var textBox = "X5694";
 
 /*
     -------------------
@@ -18,8 +18,8 @@ var textBox = 'X5999';
     -------------------
 */
 
-var isCleared = "X8614"
-var timeoutTimer = "X5462"
+var isCleared = "X1368"
+var timeoutTimer = "X8987"
 
 /*
     -----------------
@@ -36,11 +36,11 @@ function elementsByClass(elementClass) {
 }
 
 function leaveChat() {
-    X2661('X3836');
+    X2041("X2398");
 }
 
 function closePopup() {
-    X8071();
+    X3207();
 }
 
 function postMessage(message) {
@@ -49,7 +49,7 @@ function postMessage(message) {
     for (var tag in HTMLTags) {
         message = message.replace(HTMLTags[tag],ChatzyTags[tag]);
     }
-    X4727(message);
+    X8532(message);
 }
 
 function closeChat() {
@@ -61,7 +61,7 @@ function openChat() {
 }
 
 function toggleChatLock() {
-    if(X5170.X5289) { //Variable for whether the chat is locked, of course.
+    if(X7759.X1874) { //Variable for whether the chat is locked, of course.
         postMessage("/open");
     } else {
         postMessage("/close");
@@ -77,7 +77,7 @@ function privateMessage(name, message) {
 }
 
 function globalMessage(message) { //Note, only sends the message to online users.
-    var users = X3884.split("\n");
+    var users = X8696.split("\n");
     for (var i = 1; i <= users[0]; i++) {
         user = users[i].split(" ");
         privateMessage(user[0], message);   //Replace the 0 with other numbers to grab different values. 2 is last leave/exit, 4 is status, 5 is location.
@@ -87,7 +87,7 @@ function globalMessage(message) { //Note, only sends the message to online users
 function editRoomBoard(message, method, key) {  //Method is the style of editing to use. Options are: 0/default, overwrite. 1, append. 2, prepend. 3, replace.
     postMessage("/rb");
     setTimeout(function() {
-        var BoardMessage = elementByID("X6196");
+        var BoardMessage = elementByID("X3755");
         switch (method) {
             case 1:
                 BoardMessage.value = BoardMessage.value + "\n" + message;
@@ -103,17 +103,17 @@ function editRoomBoard(message, method, key) {  //Method is the style of editing
             default:
                 BoardMessage.value = message;
         }
-        X565.onclick();
+        X5999.onclick();
     }, 150);
 }
 
 //Requires Re-Init. Fix that?
 function changeName(name) {
-    X8752('X6610');
+   X8234('X3489');
 
     setTimeout(function() {
-        X4964.value = name;
-        X2575.onsubmit();
+        X7758.value = name;
+        X6791.onsubmit();
     }, 1000);
 }
 
@@ -129,5 +129,5 @@ function highlightTab(elIn, classIn, confusingBool) {
 }
 
 function generateTab(picName, clickFunc, displayText, isChecked, isLocked) {
-    return '<A href="#" onClick="' + clickFunc + 'return false;"' + (isLocked ? ' class="X8949"' : "") + '>' + (isChecked ? "<SPAN style='float:right;margin:0 4px 0 0;'>&nbsp;&#10003;</SPAN>" : "") + (picName ? '<IMG src="/elements/icon17/' + picName + '.png">' : "") + displayText + '</A>';
+    return '<A href="#" onClick="' + clickFunc + 'return false;"' + (isLocked ? ' class="X6614"' : "") + '>' + (isChecked ? "<SPAN style='float:right;margin:0 4px 0 0;'>&nbsp;&#10003;</SPAN>" : "") + (picName ? '<IMG src="/elements/icon17/' + picName + '.png">' : "") + displayText + '</A>';
 }
