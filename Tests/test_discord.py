2578506d510780927d87ceb70f8c5dab616c332f
Rune Tynan <runetynan@gmail.com>
2017-09-08 09:58:26 -0400

    Import playing

--- Tests/test_discord.py
+++ Tests/test_discord.py
@@ -1,9 +1,11 @@
 import pytest
-import Discord.Talos
+import importlib
+
+Talos = importlib.import_module("Discord.Talos")
 
 
 def test_extension_load():
-    bot = Discord.Talos.Talos()
+    bot = Talos.Talos()
     bot.load_extensions()
     assert len(bot.extensions) == 3, "Didn't load 3 extensions"
     assert "Commands" in bot.extensions, "Didn't load Commands extension"
