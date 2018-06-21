c650d42e9ebe9f72affcb2e1fef3c7933e72cddc
CraftSpider <runetynan@gmail.com>
2018-06-20 23:49:38 -0400

    Handle the case where the token file doesn't exist

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -21,7 +21,10 @@ def testlos():
 @pytest.fixture(scope="module")
 def testlos_m(request):
 
-    nano_login, btn_key, cat_key = dtalos.load_nano_login(), dtalos.load_btn_key(), dtalos.load_cat_key()
+    try:
+        nano_login, btn_key, cat_key = dtalos.load_nano_login(), dtalos.load_btn_key(), dtalos.load_cat_key()
+    except FileNotFoundError:
+        nano_login, btn_key, cat_key = ["", ""], "", ""
 
     testlos = dtalos.Talos(nano_login=nano_login, btn_key=btn_key, cat_key=cat_key)
     testlos._connection = dfacts.get_state()
