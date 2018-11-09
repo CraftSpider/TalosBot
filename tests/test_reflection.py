6ca57d234770c3bf9d1ded2a0440e105944f40b2
Rune Tynan <runetynan@gmail.com>
2018-11-09 16:27:46 -0500

    filter around class_attributes

--- tests/test_reflection.py
+++ tests/test_reflection.py
@@ -6,7 +6,8 @@ import pkgutil
 import importlib
 
 
-def isdocable(member):
+def isdocable(attr):
+    member = attr.object
     if isinstance(member, commands.Command) or isinstance(member, dutils.EventLoop):
         return True
     return inspect.iscoroutinefunction(member) or inspect.isfunction(member) or inspect.ismethod(member) or \
@@ -36,18 +37,28 @@ def get_unique_member(base_class):
     return predicate
 
 
-def getdeclared(type):
-    type.mro()
-    pass  # TODO: filter getmembers
+def getdeclared(type, predicate=None):
+    if not inspect.isclass(type):
+        type = type.__class__
+    attrs = inspect.classify_class_attrs(type)
+    results = []
+    for item in attrs:
+        if item.defining_class != type:
+            continue
+        elif not predicate or predicate(item):
+            results.append(item)
+    print(results)
+    return results
 
 
 def test_command_docs(testlos):
-    type = testlos.__class__
-    for name, member in inspect.getmembers(type, get_unique_member(type)):
-        assert inspect.getdoc(member) is not None, "Talos method missing docstring"
+    for attr in getdeclared(testlos, isdocable):
+        assert inspect.getdoc(attr.object) is not None, "Talos method missing docstring"
     for cog in testlos.cogs:
         cog = testlos.cogs[cog]
-        for name, member in inspect.getmembers(cog, get_unique_member(cog)):
+        for attr in getdeclared(cog, isdocable):
+            name = attr.name
+            member = attr.object
             if isinstance(member, commands.Command):
                 assert inspect.getdoc(member.callback) is not None, f"Cog command {name} missing docstring"
                 assert member.description is not "", f"Cog command {name} missing description"
