34d46bf0f7384e300b5960886b713ccedb33657d
CraftSpider <runetynan@gmail.com>
2019-10-07 12:33:52 -0400

    Add TODO

--- tests/test_discord/test_user_commands.py
+++ tests/test_discord/test_user_commands.py
@@ -8,4 +8,6 @@ pytestmark = pytest.mark.usefixtures("testlos_m")
 
 
 async def test_colour():
+    # TODO: Setup bot permissions
+    # await message("^colour #8F008F")
     pytest.skip("Colour testing not yet implemented")
