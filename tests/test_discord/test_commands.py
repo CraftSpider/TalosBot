2bd2ef045baedb0237daa632a84013d18204f9ea
CraftSpider <runetynan@gmail.com>
2019-10-11 14:15:39 -0400

    Fill out quote and PW tests

--- tests/test_discord/test_commands.py
+++ tests/test_discord/test_commands.py
@@ -1,12 +1,28 @@
 
 import pytest
 import asyncio
+import functools
 import discord.ext.commands as commands
 
-from discord.ext.test import message, verify_message, verify_embed, empty_queue, sent_queue, verify_file
+from discord.ext.test import (
+    message, verify_message, verify_embed, empty_queue, sent_queue, verify_file, get_config, backend
+)
 
 
 pytestmark = pytest.mark.usefixtures("testlos_m")
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup(testlos_m):
+    global admin, admess
+    guild = get_config().guilds[0]
+    admin = backend.make_member(
+            backend.make_user("AdminUser", "0001"),
+            guild,
+            roles=[backend.make_role("Admin", guild, permissions=8)]
+        )
+
+    admess = functools.partial(message, member=admin)
 
 
 async def test_choose():
@@ -93,11 +109,74 @@ async def test_ping():
 
 
 async def test_productivitywar():
-    pytest.skip("Productivity War testing not yet implemented")
+    member = get_config().members[0]
+    member1 = get_config().members[1]
+
+    await message("^pw")
+    verify_message()
+    await message("^pw end")
+    verify_message("There's currently no PW going on. Would you like to **create** one?")
+    await message("^pw join")
+    verify_message("No PW to join. Maybe you want to **create** one?")
+    await message("^pw leave")
+    verify_message("No PW to leave. Maybe you want to **create** one?")
+
+    await message("^pw create")
+    verify_message("Creating a new PW.")
+    await message("^pw create")
+    verify_message("There's already a PW going on. Would you like to **join**?")
+    await message("^pw join")
+    verify_message("You're already in this PW.")
+    await message("^pw leave", member=1)
+    verify_message("You aren't in the PW! Would you like to **join**?")
+    await message("^pw join", member=1)
+    verify_message(f"User {member1.display_name} joined the PW.")
+    await message("^pw start")
+    verify_message("Starting PW")
+    await message("^pw leave", member=1)
+    verify_message(f"User {member1.display_name} left the PW.")
+    await message("^pw leave", member=1)
+    verify_message(f"You've already left this PW! Are you going to **end** it?")
+    await message("^pw leave")
+    verify_message(f"User {member.display_name} left the PW.")
+    verify_message("Ending PW.")
+    verify_embed()
+
+    await message("^pw create")
+    verify_message()
+    await message("^pw end")
+    verify_message("Deleting un-started PW.")
+
+    await message("^pw create")
+    verify_message()
+    await message("^pw start")
+    verify_message()
+    await message("^pw end")
+    verify_message("Ending PW.")
+    verify_embed()
 
 
-async def test_quote():
-    pytest.skip("Quote testing not yet implemented")
+async def test_quote(database):
+    await message("^quote")
+    verify_message("There are no quotes available for this guild")
+    await message("^quote TestUser Hello World!")
+    verify_message("You don't have permission to add quotes, sorry")
+    await admess("^quote TestUser Hello World!")
+    verify_message("Quote from TestUser added!")
+
+    await message("^quote")
+    verify_embed()
+    await message("^quote list")
+    verify_embed()
+
+    with pytest.raises(commands.CheckFailure):
+        await message("^quote remove 1")
+    await empty_queue()
+    await admess("^quote remove 1")
+    verify_message("Removed quote 1")
+    await message("^quote")
+    verify_message("There are no quotes available for this guild")
+
 
 
 async def test_roll():
